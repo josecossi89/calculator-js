adc4c66ff76b74b07d4d0f39c4e6367872e87710
Jose Cossi <josecossib@gmail.com>
2021-07-14 07:06:04 -0300

    agregados metodos de calculadora en la clase Calculadora.js

---
+++ scripts-js/main.js
@@ -0,0 +1,4 @@
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorActual = document.getElementById("valor-actual");
+const botonesNumeros = document.querySelectorAll(".numeros");
+const botonesOperadores = document.querySelectorAll(".operador");
